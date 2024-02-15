07f1298d295c2845bae6bc4ea79f1456a79c3bc5
David <yugiohgxd600@gamil.com>
2024-02-15 04:48:59 +0200

    minify html & css

--- js/mian.js
+++ js/mian.js
@@ -2,21 +2,17 @@ const sidebar = document.querySelector(".sidebar");
 const menu = document.querySelector(".menu");
 const imgMain = document.querySelector(".imgMain");
 const project = document.querySelector(".project");
-
 function showSidebar() {
   sidebar.style.display = "flex";
   menu.style.display = "none";
 }
-
 function hideSidebar() {
   sidebar.style.display = "none";
   menu.style.display = "block";
 }
-
 function phones(phone) {
   imgMain.src = phone;
 }
-
 function colors(color) {
   project.style.background = color;
 }
